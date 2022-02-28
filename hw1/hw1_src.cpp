931799f550938204facf627b340c3eae53d4514c
Nivalis-Git <s.n.shterenberg@gmail.com>
2022-02-28 16:52:34 -0500

    Solved the infinite loop that occured when attempting to fork C before printing with P
    Adding a sleep command before printing with P gives enough time to fork C

--- hw1/hw1_src.cpp
+++ hw1/hw1_src.cpp
@@ -9,26 +9,36 @@
 
 int main()
 {
-	// I. Initialize out stream
+// I. Initialize out stream
 	std::ofstream outFile("hw1_output.txt");
 	
 	
-	// II. Split execution between parent (P) and child (C) processes
-	int pid = fork();  // duplicate current process P (P --> C)
+// II. Split execution between parent (P) and child (C) processes
+	int pid = fork();
 	
+	// i. branch for parent process (P)
+	if (pid > 0)
+	{
+		sleep(1);
+		std::cout << "I am the grandparent process. PID is " << getpid() << "\n";
+		outFile.close();
+		std::cout << "SIGCONT not yet sent" << "\n";
+		kill(-1, SIGCONT); std::cout << "SIGCONT sent" << "\n";
+		wait(NULL);
+	}
 	
 	// ii. branch for child process (C)
 	if (pid == 0)
 	{
-		//raise(SIGSTOP);
-		int pid_c = fork();  // duplicate current process C (c --> GC)
+		int pid_c = fork();
+		std::cout << getpid() << ": SIGSTOP not yet raised" << "\n";
+		raise(SIGSTOP); std::cout << getpid() << ": SIGSTOP raised" << "\n";
 		
 		// a. branch for process C
 		if (pid_c > 0)
 		{
-			std::cout << "SIGSTOP not yet sent" << "\n";
-			raise(SIGSTOP);
-			//outFile << "I am the parent process. PID is " << getpid() << "\n";
+			//std::cout << "(C) SIGSTOP not yet raised" << "\n";
+			//raise(SIGSTOP); std::cout << "(C) SIGSTOP raised" << "\n";
 			std::cout << "I am the parent process. PID is " << getpid() << "\n";
 			outFile.close();
 			kill(-1, SIGCONT);
@@ -38,37 +48,14 @@ int main()
 		// b. branch for grandchild process (GC)
 		if (pid_c == 0)
 		{
-			std::cout << "SIGSTOP not yet sent" << "\n";
-			raise(SIGSTOP);
-			//outFile << "I am the grandchild process. PID is " << getpid() << "\n";
+			//std::cout << "(GC) SIGSTOP not yet raised" << "\n";
+			raise(SIGSTOP); //std::cout << "(GC) SIGSTOP raised" << "\n";
 			std::cout << "I am the grandchild process. PID is " << getpid() << "\n";
 			outFile.close();
 		}
-		
-		// iii. If child was not created, notify user of error
-		if (pid_c == -1)
-		{
-			std::cout << "Error (Process C): child could not be created" << "\n";
-		}
-	}
-	
-	// i. branch for parent process (P)
-	if (pid > 0)
-	{
-		//outFile << "I am the grandparent process. PID is " << getpid() << "\n";
-		std::cout << "I am the grandparent process. PID is " << getpid() << "\n";
-		outFile.close();
-		kill(-1, SIGCONT); std::cout << "SIGCONT sent" << "\n";
-		wait(NULL);
-	}	
-	
-	// iii. If child was not created, notify user of error
-	if (pid == -1)
-	{
-		std::cout << "Error (Process P): child could not be created" << "\n";
 	}
 	
 	
-	// III. End of program
+// III. End of program
 	return 0;
 }
