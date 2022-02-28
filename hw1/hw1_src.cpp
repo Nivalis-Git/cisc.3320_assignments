bfd1c67acc71ba8e4b7109c67f42a28b8a60360d
Nivalis-Git <s.n.shterenberg@gmail.com>
2022-02-28 15:27:48 -0500

    Final revision
    Comments modified

--- hw1/hw1_src.cpp
+++ hw1/hw1_src.cpp
@@ -1,71 +1,56 @@
 // Assignment 1, 3320 CISC
 // Shterenberg, Simon
 
-#include <iostream>
+
 #include <fstream>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
 
+
 int main()
 {
-	// I. Initialize out stream
+// I. Initialize out stream
 	std::ofstream outFile("hw1_output.txt");
 	
 	
-	// II. Split execution between parent (P) and child (C) processes
-	int pid = fork();  // duplicate current process P (P --> C)
+// II. Split execution between parent (P) and child (C) processes
+	int pid = fork();
 	
 	// i. branch for parent process (P)
 	if (pid > 0)
 	{
 		outFile << "I am the grandparent process. PID is " << getpid() << "\n";
 		outFile.close();
-		kill(-1, SIGCONT); std::cout << "SIGCONT sent" << "\n";
+		kill(-1, SIGCONT);  // continues C
 		wait(NULL);
 	}	
 	
 	// ii. branch for child process (C)
 	if (pid == 0)
 	{
-		std::cout << "SIGSTOP not yet sent" << "\n";
-		raise(SIGSTOP);
-		std::cout << "gone past SIGSTOP" << "\n";
-		int pid_c = fork();  // duplicate current process C (c --> GC)
+		raise(SIGSTOP);  // stop process C so that P can print first
+		int pid_c = fork();
 		
 		// a. branch for process C
 		if (pid_c > 0)
 		{
-			std::cout << "In C branch" << "\n";
 			outFile << "I am the parent process. PID is " << getpid() << "\n";
 			outFile.close();
-			kill(-1, SIGCONT);
+			kill(-1, SIGCONT);  // continues GC
 			wait(NULL);
 		}
 		
 		// b. branch for grandchild process (GC)
 		if (pid_c == 0)
 		{
-			std::cout << "In GC Branch" << "\n";
-			raise(SIGSTOP);
+			raise(SIGSTOP);  // stop GC so that C can print first
 			outFile << "I am the grandchild process. PID is " << getpid() << "\n";
 			outFile.close();
 		}
-		
-		// iii. If child was not created, notify user of error
-		if (pid_c == -1)
-		{
-			std::cout << "Error (Process C): child could not be created" << "\n";
-		}
-	}
-	
-	// iii. If child was not created, notify user of error
-	if (pid == -1)
-	{
-		std::cout << "Error (Process P): child could not be created" << "\n";
 	}
 	
 	
-	// III. End of program
+// III. End of program
 	return 0;
 }
