53780bc117fadd2cfd7ce6c0cd8b17b9e1f89e85
Nivalis-Git <s.n.shterenberg@gmail.com>
2022-02-28 00:23:16 -0500

    The raise() after the second fork causes an infinite loop

--- hw1/hw1_src.cpp
+++ hw1/hw1_src.cpp
@@ -13,19 +13,11 @@ int main()
 	// II. Split execution between parent (P) and child (C) processes
 	int pid = fork();  // duplicate current process P (P --> C)
 	
-	// i. branch for parent process (P)
-	if (pid > 0)
-	{
-		std::cout << "I am the grandparent process. PID is " << getpid() << "\n";
-		kill(-1, SIGCONT);
-		waitpid(0, NULL, 0);
-	}
-	
 	// ii. branch for child process (C)
 	if (pid == 0)
 	{
-		raise(SIGSTOP);
 		int pid_c = fork();  // duplicate current process C (c --> GC)
+		raise(SIGSTOP);
 		
 		// a. branch for process C
 		if (pid_c > 0)
@@ -42,6 +34,14 @@ int main()
 			std::cout << "I am the grandchild process. PID is " << getpid() << "\n";
 		}
 	}
+	
+	// i. branch for parent process (P)
+	if (pid > 0)
+	{
+		std::cout << "I am the grandparent process. PID is " << getpid() << "\n";
+		kill(-1, SIGCONT);
+		waitpid(0, NULL, 0);
+	}	
 	
 	
 	// III. Close the program
