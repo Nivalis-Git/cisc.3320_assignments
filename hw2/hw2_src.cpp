29b19b723223d276025492ae27b7e66b1e39df82
Simon <s.n.shterenberg@gmail.com>
2022-03-16 21:09:27 -0400

    Saving progress to leave class

--- hw2/hw2_src.cpp
+++ hw2/hw2_src.cpp
@@ -15,7 +15,7 @@
 #include <random>
 
 
-void unlink(std::vector<std::string> &vSem);
+void unlink(std::vector<char*> &vSem);
 
 
 int main()
@@ -31,29 +31,35 @@ int main()
 	std::vector<char*> vRsrc;  // a vector to associate the semaphores with an index
 	vRsrc.insert( vRsrc.end(), {"printer", "plotter", "scanner"} );
 	
-	int num;
-	sem_getvalue(sem_printer, &num);
-	outFile << num << "\n";
-	
 	std::random_device rnd;
 	
 	
 // .
+	//for (int i = 0; i < 4; i)
+	
 	int pid = fork();
 	
 	if (pid == 0)
 	{
-		std::uniform_int_distribution<int> dstr(0,2);
-		char* rsrc_name = vRsrc[dstr(rnd)];
+		std::uniform_int_distribution<int> dstr(1,3);
+		char* rsrc_name = vRsrc[dstr(rnd)-1];
 		sem_t *rsrc_sem = sem_open(rsrc_name, 0);
-		int rsrc_qty;
-		sem_getvalue(rsrc_sem, &rsrc_qty);
 		
-		outFile << "PID: " << getpid() << "\t" << "Resource Type: " << rsrc_name << "\n";
-		outFile << rsrc_qty << "\n";
+		outFile << "Child PID: " << getpid()
+		        << "\t\t" << "Resource Type: " << rsrc_name << "\n";
 		
+		outFile << "Child PID: " << getpid();		
+		while (sem_wait(rsrc_sem) != 0)
+		{
+		  outFile << "\t\t" << "Request Status: Failed" << "\n";
+		  sleep(dstr(rnd)+1);
+		}
+		outFile << "\t\t" << "Request Status: Succeeded" << "\n\n";
+		
+		sleep(dstr(rnd));
+		sem_post(rsrc_sem);
+		sem_close(rsrc_sem);
 		outFile.close();
-		//sem_close(rsrc_sem);
 	}
 	
 	if (pid > 0)
